2b44d44fb9664e8ea9d49dceec3b4e49e992e293
MarcosValens <marcosvalens@gmail.com>
2021-09-09 11:54:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-jvhnta-uat8j4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-jvhnta-uat8j4)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+  @Input() product!: Product;
+  @Output() notify = new EventEmitter();
+  @Output() notify2 = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
